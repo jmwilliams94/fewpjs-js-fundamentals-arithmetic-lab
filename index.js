c71f7b6ef7016b31305f3c4937d11af15693067c
Jordan <jmwilliams94@icloud.com>
2022-03-28 10:33:16 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+ let num1 = 2
+ let num2 = 31
+ let num3 = 5
+ let num4 = 19
+
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = (num4 % num3)
+   
+let max = Math.max(1, 2, 3, 4, 5, 20)
+
+ 
